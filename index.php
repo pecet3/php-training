0f54b97846bf80b42f9cca8e894d7d3fd7860413
pecet <pecet3107@gmail.com>
2024-02-29 11:47:57 +0100

    oki

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 echo "hello world <br>";
 echo "hello mom";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <button>Order pizza</button>
+</body>
+
+</html>
